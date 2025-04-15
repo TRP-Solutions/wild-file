42cf13530ef2036979996f590c141356d2c71613
JacobNeergaard <neergaard@kananga.dk>
2025-04-15 13:54:03 +0200

    Remove db defaults

--- sample/dbschema.sql
+++ sample/dbschema.sql
@@ -3,12 +3,12 @@ USE `wildfile`;
 
 CREATE TABLE `files` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-	`name` varchar(100) NOT NULL DEFAULT '',
-	`mime` varchar(128) NOT NULL DEFAULT '',
-	`size` int(10) unsigned NOT NULL DEFAULT 0,
-	`checksum` char(64) NOT NULL DEFAULT '',
-	`address` varchar(15) NOT NULL DEFAULT '',
-	`created` datetime DEFAULT NULL,
+	`name` varchar(100) NOT NULL,
+	`mime` varchar(128) NOT NULL,
+	`size` int(10) unsigned NOT NULL,
+	`checksum` char(64) NOT NULL,
+	`address` varchar(15) NOT NULL,
+	`created` datetime NOT NULL,
 	`thumbnail` varchar(100) DEFAULT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
